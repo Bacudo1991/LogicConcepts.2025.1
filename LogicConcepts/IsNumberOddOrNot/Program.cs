5c4930bf132529aa9a93bcf788b050861e3c1fb9
ITM <ITM@DESKTOP-6GH284K>
2025-08-15 10:51:49 -0500

    Complete exercice 01

---
+++ LogicConcepts/IsNumberOddOrNot/Program.cs
@@ -0,0 +1,29 @@
+﻿var numberString = string.Empty;
+do
+{
+    Console.Write("Ingrese un numero o 'S' para salir: ");
+    numberString = Console.ReadLine();
+    if (numberString == "S" || numberString == "s")
+    {
+        break;
+    }
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El numero {numberInt}, es par"); // El simbolo de interpolación $ permite incluir variables directamente en la cadena.
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numberInt}, es impar");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"El valor ingresado: {numberString} no es un numero entero."); 
+    }
+} while (numberString != "S" && numberString != "s");
+Console.WriteLine(":::::: GAME OVER ::::::");
+
+
