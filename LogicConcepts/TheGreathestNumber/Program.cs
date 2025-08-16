eda644f7d11066cf44524716e6ae647150ee369a
ITM <ITM@DESKTOP-6GH284K>
2025-08-16 18:37:41 -0500

    Complete exercice 02

--- LogicConcepts/TheGreathestNumber/Program.cs
+++ LogicConcepts/TheGreathestNumber/Program.cs
@@ -1,30 +1,37 @@
-﻿do
+﻿do 
 {
-    int n1, n2, n3;
-    Console.WriteLine(":::: Ingrese tres numeros ::::");
-    Console.Write("Ingrese el primer numero: ");
-    n1 = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Ingrese el segundo numero: ");
-    n2 = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Ingrese el tercer numero: ");
-    n3 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Ingrese 3 números para saber cual es el mayor (CTRL + C para salir)\n");
+
+    ;
+    Console.Write("Ingrese el primer número: ");
+    int n1 = int.Parse(Console.ReadLine()!);
+    Console.Write("Ingrese el segundo número: ");
+    int n2 = int.Parse(Console.ReadLine()!);
+    Console.Write("Ingrese el tercer número: ");
+    int n3 = int.Parse(Console.ReadLine()!);
+
 
     if (n1 > n2 && n1 > n3)
     {
-        Console.WriteLine("El numero mayor es: " + n1);
+        Console.WriteLine($"El número mayor es: {n1}");
     }
     else if (n2 > n1 && n2 > n3)
     {
-        Console.WriteLine("El numero mayor es: " + n2);
+        Console.WriteLine($"El número mayor es: {n2}");
     }
     else if (n3 > n1 && n3 > n2)
     {
-        Console.WriteLine("El numero mayor es: " + n3);
+        Console.WriteLine($"El número mayor es: {n3}");
     }
-    else
+    else if (n1 == n2 && n2 == n3)
     {
-        Console.WriteLine("Hay numeros iguales");
+        Console.WriteLine("Los tres números son iguales, no se puede determinar un número mayor");
     }
-}while (true);
+    else if (n1 == n2 || n2 == n3 || n1 == n3)
+    {
+        Console.WriteLine("Hay al menos dos números iguales, no se puede determinar un número mayor.");
+    }
+} while (true);
+
 
 
