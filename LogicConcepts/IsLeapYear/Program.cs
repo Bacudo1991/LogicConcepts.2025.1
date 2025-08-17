02ae2079c35715ac0e6feff9440d6454cf503dc2
ITM <ITM@DESKTOP-6GH284K>
2025-08-17 10:07:28 -0500

    Complete exercice 05

--- LogicConcepts/IsLeapYear/Program.cs
+++ LogicConcepts/IsLeapYear/Program.cs
@@ -1,15 +1,29 @@
 ﻿do
-{
+{    
     Console.WriteLine("Ingrese un año para saber si es bisiesto\n");
     Console.Write("Ingrese el año: ");
+    var currentYear = DateTime.Now.Year;
+    var message = string.Empty;
     int year = int.Parse(Console.ReadLine()!);
-    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    if (year == currentYear)
     {
-        Console.WriteLine($"{year} es un año bisiesto.\n");
+        message = "es";
+    }
+    else if (year < currentYear)
+    {
+        message = "fue";
     }
     else
     {
-        Console.WriteLine($"{year} no es un año bisiesto.\n");
+        message = "será";
+    }
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    {
+        Console.WriteLine($"El año {year}, {message} un año bisiesto.\n");
+    }
+    else
+    {
+        Console.WriteLine($"El año {year}, no {message} un año bisiesto.\n");
     }
     Console.WriteLine("Si desea salir presione CTRL + C\n");
 } while (true);
