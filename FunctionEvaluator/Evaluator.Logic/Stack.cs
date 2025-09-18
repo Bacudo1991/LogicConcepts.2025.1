01a71ada07e46bbcead9788ee700d5b3a4522a18
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-09-18 12:58:32 -0500

    "Updates Evaluator"

---
+++ FunctionEvaluator/Evaluator.Logic/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluator.Logic
+{
+    public class Stack<T>
+    {
+        private T[] _stack;
+        private int _top;
+
+        public Stack(int n)
+        {
+            N = n;
+            _stack = new T[N];
+        }
+
+        public int N { get; }
+
+        public bool IsEmpty => _top == 0;
+
+        public bool IsFull => _top == N;
+
+        public void Push(T item)
+        {
+            if (IsFull)
+            {
+                throw new Exception("Stack Full");
+            }
+
+            _stack[_top] = item;
+            _top++;
+        }
+
+        public T Pop() 
+        {
+            if (IsEmpty)
+            {
+                throw new Exception("Stack Empty");
+            }
+
+            var item = _stack[_top];
+            _top--;
+            return item;
+        }
+    }
+}
---
+++ FunctionEvaluator/Evaluator.UI.Console/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using Evaluator.Logic;
+
+Console.WriteLine("Expresions Evaluator");
+var expresion = "4*5/(4+6)";
+Console.WriteLine($"{expresion} = {MyEvaluator.Evaluate(expresion)}");
